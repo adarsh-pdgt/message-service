4b0f6084fe43b4c5f439fce88bb71981dce250c3
adarsh-pdgt <adarsh@primedigital.tech>
2023-09-21 17:20:25 +0530

    chore: updated api

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const config = require("./config/db.config");
 const {createOneToOneMessageRoom} = require("./services/addParticipant.service")
 const {sendMesage} = require("./services/message.service");
 const {ObjectRooms, Message} = require("./models/messages.models");
-const { instrument } = require("@socket.io/admin-ui");
+const { v4: uuidv4 } = require('uuid');
 const redisClient = createClient({ host: "localhost", port: 6379 });
 redisClient.connect();
 
@@ -30,17 +30,13 @@ const io = new Server(server, {
   adapter: createAdapter(redisClient)
 });
 
-instrument(io, {
-  auth: false
-});
-
-
 
 io.on('connection', (socket) => {
   user = socket.handshake.headers["x-authenticated-user"];
+  user = JSON.parse(user)
 
-  socket.on("join-room", (data) => {
-    const {userId} = data;
+  socket.on("join-room", () => {
+    const userId = user.id;
     ObjectRooms.findOne({objectId: userId}).exec().then(
       (res) => {
         try{
@@ -55,14 +51,16 @@ io.on('connection', (socket) => {
   });
 
   socket.on("create-dm-room", (data) => {
-    const {room, userId, participantId} = data;
-    createOneToOneMessageRoom(userId, room, participantId);
+    const userId = user.id;
+    const {participantId} = data;
+    roomId = uuidv4();
+    createOneToOneMessageRoom(userId, roomId, participantId);
   });
 
   socket.on("send-message", (data) => {
-    const {roomId, message, sender} = data;
+    const {roomId, message} = data;
+    const sender = user.id;
     const response = sendMesage(roomId, sender, message);
-    console.log(response);
     io.sockets.in(roomId).emit('live-messages', response);
   });
 
